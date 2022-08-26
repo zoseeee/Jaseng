750e8f02d188a8d1876bed1bf7b851b67ef20bad
jigoooo <102403987+jigoooo@users.noreply.github.com>
2022-08-26 12:53:40 +0900

    Add files via upload
    22.08.26

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+const Main = new Swiper(".Main", {
+  loop: true,
+  autoplay: true,
+  speed: 1000,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 const card = new Swiper(".card", {
     slidesPerView: 4,
     spaceBetween: 30,
     slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
 
 const review = new Swiper(".review", {
@@ -11,6 +25,11 @@ const review = new Swiper(".review", {
     spaceBetween: 30,
     slidesPerGroup: 4,
     loop: true,
-    // autoplay: true,
+    autoplay: true,
+    speed: 1000,
     loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
